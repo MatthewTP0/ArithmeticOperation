045443eaa65df9e3aa412f7beafb1d0aa3c8d7f7
Matthew Thomas Pompeo <113402358+MatthewTP0@users.noreply.github.com>
2025-04-01 18:11:21 -0400

    make test fails

--- src/test/java/soen/tutorial/ArithmeticOperationTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationTest.java
@@ -3,6 +3,7 @@ package soen.tutorial;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class ArithmeticOperationTest {
 
@@ -14,18 +15,21 @@ public class ArithmeticOperationTest {
 		ArithmeticOperation operations = new ArithmeticOperation();
 		Integer actual = operations.addOrSub(2, 6);
 		Integer expected = 8;
-		assertEquals(expected, actual);
+		//assertEquals(expected, actual);
+
+		 // Forcefully failing the test
+        	fail("Forced failure");
 
 	}
-    @Test
-    public void testSub()
-    {
+    // @Test
+    // public void testSub()
+    // {
 
-        ArithmeticOperation operations = new ArithmeticOperation();
-        Integer actual = operations.addOrSub(6, 2);
-        Integer expected = 4;
-        assertEquals(expected, actual);
+    //     ArithmeticOperation operations = new ArithmeticOperation();
+    //     Integer actual = operations.addOrSub(6, 2);
+    //     Integer expected = 4;
+    //     assertEquals(expected, actual);
 
-    }
+    // }
 
 }
